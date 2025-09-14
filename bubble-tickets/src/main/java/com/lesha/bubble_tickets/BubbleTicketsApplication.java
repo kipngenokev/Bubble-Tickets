110950f7d29ffc6e0aa0ca633307e947227cc7e1
kipngenokev <kkipngeno409@gmail.com>
2025-09-14 16:03:36 +0300

    first commit -m setting up the spring boot project

---
+++ bubble-tickets/src/main/java/com/lesha/bubble_tickets/BubbleTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.lesha.bubble_tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BubbleTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BubbleTicketsApplication.class, args);
+	}
+
+}
---
+++ bubble-tickets/src/test/java/com/lesha/bubble_tickets/BubbleTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lesha.bubble_tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BubbleTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
