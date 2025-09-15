9d2317b9393995cc9ee3f54f650a4e86259a4424
kipngenokev <kkipngeno409@gmail.com>
2025-09-15 18:50:51 +0300

    created a ticket validation class, added validation reference to ticket class

--- bubble-tickets/src/main/java/com/lesha/bubble_tickets/domain/TicketValidation.java
+++ bubble-tickets/src/main/java/com/lesha/bubble_tickets/domain/TicketValidation.java
@@ -6,41 +6,34 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Entity
-@Table(name = "tickets")
+@Table(name = "ticket_validations")
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 
-public class Ticket {
+public class TicketValidation {
 
     @Id
-    @Column(name = "id", nullable = false, updatable = false)
+    @Column(name ="id", nullable = false, updatable = false)
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(name = "status", nullable = false)
     @Enumerated(EnumType.STRING)
-    private TicketStatusEnum status;
+    private TicketValidationStatusEnum status;
+
+    @Column(name = "validation_method", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TicketValidationMethodEnum validationMethod;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "ticket_type_id")
-    private TicketType ticketType;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "purchaser_id")
-    private User purchaser;
-
-    @OneToMany(mappedBy = "ticket", cascade = CascadeType.ALL)
-    private List<TicketValidation> validations = new ArrayList<>();
-    //TODO: QR code
-
+    @JoinColumn(name = "ticket_id")
+    private Ticket ticket;
 
     @CreatedDate
     @Column(name = "created-at", updatable = false, nullable = false)
@@ -49,4 +42,5 @@ public class Ticket {
     @LastModifiedDate
     @Column(name = "updated-at", nullable = false)
     private LocalDateTime updatedAt;
+
 }
