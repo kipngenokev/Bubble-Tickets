c7c6f4de31ef3a404f0063a4285527f147e0e695
kipngenokev <kkipngeno409@gmail.com>
2025-09-15 18:09:30 +0300

    created ticket type entity, added the ticket type reference in the event class

--- bubble-tickets/src/main/java/com/lesha/bubble_tickets/domain/Event.java
+++ bubble-tickets/src/main/java/com/lesha/bubble_tickets/domain/Event.java
@@ -57,6 +57,9 @@ public class Event {
     @ManyToMany(mappedBy = "staffingEvents")
     private List<User> staff = new ArrayList<>();
 
+    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL)
+    private List<TicketType> ticketTypes = new ArrayList<>();
+
     @CreatedDate
     @Column(name = "created-at", updatable = false, nullable = false)
     private LocalDateTime createdAt;
