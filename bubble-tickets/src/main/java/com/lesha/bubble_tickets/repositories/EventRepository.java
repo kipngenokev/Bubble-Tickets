46444377b217bfab2e9f862d72d718307f9cd34f
kipngenokev <kkipngeno409@gmail.com>
2025-09-16 17:40:59 +0300

    created eventRepository interface, EventServiceImpl class and implemented create event method

---
+++ bubble-tickets/src/main/java/com/lesha/bubble_tickets/repositories/EventRepository.java
@@ -0,0 +1,11 @@
+package com.lesha.bubble_tickets.repositories;
+
+import com.lesha.bubble_tickets.domain.entities.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, UUID> {
+}
