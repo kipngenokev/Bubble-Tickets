bdaf58662423541744ecca2a1f6231cb653cde39
kipngenokev <kkipngeno409@gmail.com>
2025-09-21 15:32:30 +0300

    added repository methods to find events by id and organizerId, and implemented them on the service layer

--- bubble-tickets/src/main/java/com/lesha/bubble_tickets/services/EventService.java
+++ bubble-tickets/src/main/java/com/lesha/bubble_tickets/services/EventService.java
@@ -6,10 +6,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public interface EventService {
     Event createEvent(UUID organizerId, CreateEventRequest event);
     Page<Event> listEventsForOrganizer(UUID organizerId, Pageable pageable);
+    Optional<Event> getEventForOrganizer(UUID organizerId, UUID id);
+
 }
