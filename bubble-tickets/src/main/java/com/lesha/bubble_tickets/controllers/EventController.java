4238e3828ce141aae9ca414a9957e3d88446069b
kipngenokev <kkipngeno409@gmail.com>
2025-09-21 16:10:47 +0300

    implemented Get endpoint to retrieve event details by Id

--- bubble-tickets/src/main/java/com/lesha/bubble_tickets/controllers/EventController.java
+++ bubble-tickets/src/main/java/com/lesha/bubble_tickets/controllers/EventController.java
@@ -3,6 +3,7 @@ package com.lesha.bubble_tickets.controllers;
 import com.lesha.bubble_tickets.domain.CreateEventRequest;
 import com.lesha.bubble_tickets.domain.dtos.CreateEventRequestDto;
 import com.lesha.bubble_tickets.domain.dtos.CreateEventResponseDto;
+import com.lesha.bubble_tickets.domain.dtos.GetEventDetailsResponseDto;
 import com.lesha.bubble_tickets.domain.dtos.ListEventResponseDto;
 import com.lesha.bubble_tickets.domain.entities.Event;
 import com.lesha.bubble_tickets.mappers.EventMapper;
@@ -51,7 +52,23 @@ public class EventController {
         );
     }
 
+    @GetMapping(path = "/{eventId}")
+    public ResponseEntity<GetEventDetailsResponseDto>  getEvent(
+            @AuthenticationPrincipal Jwt jwt,
+            @PathVariable UUID eventId
+
+    ) {
+        UUID userId = parseUserId(jwt);
+        return eventService.getEventForOrganizer(userId, eventId)
+                .map(eventMapper::toGetEventDetailsResponseDto)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+
+    }
+
     private UUID parseUserId(Jwt jwt) {
         return UUID.fromString(jwt.getSubject());
     }
+
+
 }
