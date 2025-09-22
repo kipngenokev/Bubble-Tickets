496717b90507911ad275de26f76b26735b222d56
kipngenokev <kkipngeno409@gmail.com>
2025-09-22 22:36:49 +0300

    implemented the search endpoint to search for specific published events

--- bubble-tickets/src/main/java/com/lesha/bubble_tickets/controllers/PublishedEventController.java
+++ bubble-tickets/src/main/java/com/lesha/bubble_tickets/controllers/PublishedEventController.java
@@ -1,6 +1,7 @@
 package com.lesha.bubble_tickets.controllers;
 
 import com.lesha.bubble_tickets.domain.dtos.ListPublishedEventsResponseDto;
+import com.lesha.bubble_tickets.domain.entities.Event;
 import com.lesha.bubble_tickets.mappers.EventMapper;
 import com.lesha.bubble_tickets.services.EventService;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,9 +21,20 @@ public class PublishedEventController {
     private final EventService eventService;
 
     private final EventMapper eventMapper;
+
     @GetMapping
-    public ResponseEntity<Page<ListPublishedEventsResponseDto>>  listPublishedEvents(Pageable pageable) {
-        return ResponseEntity.ok(eventService.listPublishedEvents(pageable)
-                .map(eventMapper::toListPublishedEventsResponseDto));
+    public ResponseEntity<Page<ListPublishedEventsResponseDto>>  listPublishedEvents(
+            @RequestParam(required = false) String q,
+            Pageable pageable) {
+
+        Page<Event> events;
+        if(null != q && !q.trim().isEmpty()) {
+            events = eventService.searchPublishedEvents(q, pageable);
+        } else {
+            events = eventService.listPublishedEvents(pageable);
+        }
+
+        return ResponseEntity.ok(
+                 events.map(eventMapper::toListPublishedEventsResponseDto));
     }
 }
