2d357e69506dced6da1ba738e99741460283231f
kipngenokev <kkipngeno409@gmail.com>
2025-09-22 16:20:41 +0300

    implemented the delete service addded deleteEventForOrganizer in the service layer

--- bubble-tickets/src/main/java/com/lesha/bubble_tickets/services/impl/EventServiceImpl.java
+++ bubble-tickets/src/main/java/com/lesha/bubble_tickets/services/impl/EventServiceImpl.java
@@ -134,4 +134,10 @@ public class EventServiceImpl implements EventService {
         }
         return eventRepository.save(existingEvent);
     }
+
+    @Override
+    @Transactional
+    public void deleteEventForOrganizer(UUID organizerId, UUID id) {
+        getEventForOrganizer(organizerId, id).ifPresent(eventRepository::delete);
+    }
 }
