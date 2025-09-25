c4ac17e6d68e6f21c02b156f6a868f236c74cc8a
kipngenokev <kkipngeno409@gmail.com>
2025-09-25 18:38:41 +0300

    created the necessary DTOs for ticket validation

---
+++ bubble-tickets/src/main/java/com/lesha/bubble_tickets/domain/dtos/TicketValidationRequestDto.java
@@ -0,0 +1,16 @@
+package com.lesha.bubble_tickets.domain.dtos;
+
+import com.lesha.bubble_tickets.domain.entities.TicketValidationMethodEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TicketValidationRequestDto {
+    private UUID id;
+    private TicketValidationMethodEnum method;
+}
---
+++ bubble-tickets/src/main/java/com/lesha/bubble_tickets/domain/dtos/TicketValidationResponseDto.java
@@ -0,0 +1,16 @@
+package com.lesha.bubble_tickets.domain.dtos;
+
+import com.lesha.bubble_tickets.domain.entities.TicketValidationStatusEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TicketValidationResponseDto {
+    private UUID ticketId;
+    private TicketValidationStatusEnum status;
+}
