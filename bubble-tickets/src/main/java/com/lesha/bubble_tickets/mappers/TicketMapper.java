fe287966d4262ef7802af6822c9e4680ec70b3a0
kipngenokev <kkipngeno409@gmail.com>
2025-09-25 15:08:14 +0300

    created the necessary DTOs to getTicket and added them to the TicketMapper

--- bubble-tickets/src/main/java/com/lesha/bubble_tickets/mappers/TicketMapper.java
+++ bubble-tickets/src/main/java/com/lesha/bubble_tickets/mappers/TicketMapper.java
@@ -1,10 +1,12 @@
 package com.lesha.bubble_tickets.mappers;
 
+import com.lesha.bubble_tickets.domain.dtos.GetTicketResponseDto;
 import com.lesha.bubble_tickets.domain.dtos.ListTicketResponseDto;
 import com.lesha.bubble_tickets.domain.dtos.ListTicketTicketTypeResponseDto;
 import com.lesha.bubble_tickets.domain.entities.Ticket;
 import com.lesha.bubble_tickets.domain.entities.TicketType;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
@@ -12,4 +14,12 @@ public interface TicketMapper {
     ListTicketTicketTypeResponseDto toListTicketTicketTypeResponseDto(TicketType ticketType);
 
     ListTicketResponseDto toListTicketResponseDto(Ticket ticket);
+
+    @Mapping(target = "price", source = "ticket.ticketType.price")
+    @Mapping(target = "description", source = "ticket.ticketType.description")
+    @Mapping(target = "eventName",source = "ticket.ticketType.event.name")
+    @Mapping(target = "eventVenue", source = "ticket.ticketType.event.venue")
+    @Mapping(target = "eventStart", source = "ticket.ticketType.event.start")
+    @Mapping(target = "eventEnd", source = "ticket.ticketType.event.end")
+    GetTicketResponseDto toGetTicketResponseDto(Ticket ticket);
 }
