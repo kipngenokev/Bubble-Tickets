f13e9f32fa42b721a837f999e1db5578c87c94bd
kipngenokev <kkipngeno409@gmail.com>
2025-09-25 15:39:15 +0300

    added methods to getQr code from the repository and implemented them on the service layer

--- bubble-tickets/src/main/java/com/lesha/bubble_tickets/services/QrCodeService.java
+++ bubble-tickets/src/main/java/com/lesha/bubble_tickets/services/QrCodeService.java
@@ -4,7 +4,11 @@ import com.lesha.bubble_tickets.domain.entities.QrCode;
 import com.lesha.bubble_tickets.domain.entities.Ticket;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public interface QrCodeService {
     QrCode generateQrCode(Ticket ticket);
+
+    byte[] getQrCodeImageForUserAndTicket(UUID userId, UUID ticketId);
 }
--- bubble-tickets/src/main/java/com/lesha/bubble_tickets/services/impl/QrCodeServiceImpl.java
+++ bubble-tickets/src/main/java/com/lesha/bubble_tickets/services/impl/QrCodeServiceImpl.java
@@ -9,9 +9,11 @@ import com.lesha.bubble_tickets.domain.entities.QRCodeStatusEnum;
 import com.lesha.bubble_tickets.domain.entities.QrCode;
 import com.lesha.bubble_tickets.domain.entities.Ticket;
 import com.lesha.bubble_tickets.exceptions.QrCodeGenerationException;
+import com.lesha.bubble_tickets.exceptions.QrCodeNotFoundException;
 import com.lesha.bubble_tickets.repositories.QrCodeRepository;
 import com.lesha.bubble_tickets.services.QrCodeService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 
@@ -24,6 +26,7 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class QrCodeServiceImpl implements QrCodeService {
 
     private static final int QR_HEIGHT = 300;
@@ -50,6 +53,18 @@ public class QrCodeServiceImpl implements QrCodeService {
         } catch (WriterException | IOException ex) {
             throw new QrCodeGenerationException("Failed to generate QR Code", ex);
 
+        }
+    }
+
+    @Override
+    public byte[] getQrCodeImageForUserAndTicket(UUID userId, UUID ticketId) {
+        QrCode qrCode = qrCodeRepository.findByTicketIdAndTicketPurchaserId(ticketId, userId)
+                .orElseThrow(QrCodeNotFoundException::new);
+        try{
+            return Base64.getDecoder().decode(qrCode.getValue());
+        }catch(IllegalArgumentException ex) {
+            log.error("Invalid base64 QR Code for ticket Id: {}",ticketId,ex);
+            throw new QrCodeNotFoundException();
         }
     }
 
