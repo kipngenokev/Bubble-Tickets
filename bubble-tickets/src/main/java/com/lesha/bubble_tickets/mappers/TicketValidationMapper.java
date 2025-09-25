75ba07a4728387c58a8081e751505cf000926911
kipngenokev <kkipngeno409@gmail.com>
2025-09-25 18:45:24 +0300

    added the mapping method to the TicketValidationMapper

---
+++ bubble-tickets/src/main/java/com/lesha/bubble_tickets/mappers/TicketValidationMapper.java
@@ -0,0 +1,14 @@
+package com.lesha.bubble_tickets.mappers;
+
+import com.lesha.bubble_tickets.domain.dtos.TicketValidationResponseDto;
+import com.lesha.bubble_tickets.domain.entities.TicketValidation;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface TicketValidationMapper {
+
+    @Mapping(target = "ticketId", source = "ticket.id")
+    TicketValidationResponseDto toTicketValidationResponseDto(TicketValidation ticketValidation);
+}
